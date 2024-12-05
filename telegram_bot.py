d13eed12f34138c86716ccc29088c4704e46a5cb
Hard-Working-Dimka <152815674+Hard-Working-Dimka@users.noreply.github.com>
2024-12-06 00:17:21 +0300

    Publish photo to Telegram channel

--- telegram_bot.py
+++ telegram_bot.py
@@ -6,9 +6,14 @@ def main():
     env = Env()
     env.read_env()
     bot = telegram.Bot(token=env('TG_API_KEY'))
-    print(bot.get_me())
 
-    bot.send_message(chat_id='@kfjfisnwhfiwjd', text="I'm sorry Dave I'm afraid I can't do that.")
+    chat_id = '@kfjfisnwhfiwjd'
+
+    #  bot.send_message(chat_id=chat_id, text="I'm sorry Dave I'm afraid I can't do that.")
+    bot.send_document(chat_id=chat_id,
+                      document=open(
+                          'D:\\Projects\\PYTHON\\Lessons\\Web Services API\\API_lesson_5\\images\\nasa_apod_0.jpeg',
+                          'rb'))  # TODO: использовать os.path.join
 
 
 if __name__ == '__main__':
